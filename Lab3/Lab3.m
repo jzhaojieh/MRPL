5b593f678037f51d8971e732473f71ccc88fbed2
buckenhampalace <42877790+buckenhampalace@users.noreply.github.com>
2018-09-17 21:40:54 -0400

    Add files via upload

--- Lab3/Lab3.m
+++ Lab3/Lab3.m
@@ -15,7 +15,7 @@ global newenc;
 wbase = .088;
 vt = 0.2; sf = 1; tf = sf/vt;
 ktheta = (2*pi)/sf; kk = 15.1084;
-ks = 3; Tf = ks * tf;
+ks = 2; Tf = ks * tf;
 
 % ####### FOR SIM ##############
 % start = tic;
@@ -34,71 +34,53 @@ prevy = robot.encoders.LatestMessage.Vector.Y;
 % ##### Not Sim ##################
 t = 0;
 % ################################
-oldenc = 0;
+oldenc = [0,0,0];
 global myplot;
 x = 0;
 y = 0;
 th = 0;
-% myplot = plot(xArr, yArr, 'b-');
-% xlim([0.0 0.5]);
-% ylim([0.0 0.5]);
-% robot.sendVelocity(.2, .2);
+
 while (t < Tf)
-    if oldenc == 0
-       oldenc = newenc;
-       continue
-    end
     preval = curval;
     st = vt*t/ks;
     angle = ktheta * st;
 
     dt = newenc(3) - oldenc(3);
     
-    
-    k = (kk / ks)*sin(angle)
+    k = (kk / ks)*sin(angle);
     omegat = k * vt;
     vr = vt + (0.044) * omegat;
-%     vr = (newenc(2) - oldenc(2))/dt;
     vl = vt - (0.044) * omegat;
-%     vl = (newenc(1) - oldenc(1))/dt;
     vlArr = [vlArr vl];
     vrArr = [vrArr vr];
     
     robot.sendVelocity(vl, vr)
-%     T = toc(start);
-   
     omega = (vr-vl)/wbase;
     V = (vr+vl)/2;
-%     vr = V + (.044)*omega
-%     vl = V - (.044)*omega
-%     robot.sendVelocity(vl, vr);
     encoderx = robot.encoders.LatestMessage.Vector.X;
     encodery = robot.encoders.LatestMessage.Vector.Y;
     dx = encoderx - prevx;
     dy = encodery - prevy;
     prevx = encoderx;
     prevy = encodery;
-    dt = abs(newenc(3) - oldenc(3));
-    vlactual = dx/dt;
-    vractual = dy/dt;
-    Vactual = (vlactual+vractual)/2;
-    omegaActual = (vractual-vlactual)/.088;
+
+    if dt ~= 0
+        vlactual = dx/dt;
+        vractual = dy/dt;
+        Vactual = (vlactual+vractual)/2;
+        omegaActual = (vractual-vlactual)/.088;
+        th = th + omegaActual*dt;
+        x = x + Vactual*cos(th)*dt;
+        y = y + Vactual*sin(th)*dt;
+        xArr = [xArr, x];
+        yArr = [yArr, y];
+        thArr = [thArr, th];
+        plot(yArr, xArr);
+        title("Robot Encoder Data");
+        xlabel('X(m)');
+        ylabel('Y(m)');
+    end
     
-    th = th + omegaActual*dt;
-    x = x + Vactual*cos(th)*dt;
-    y = y + Vactual*sin(th)*dt;
-%     th = th + omega * dt;
-%     x = x + V*cos(th)*dt;
-%     y = y + V*sin(th)*dt;
-    xArr = [xArr, x];
-    yArr = [yArr, y];
-    thArr = [thArr, th];
-    plot(yArr, xArr);
-%     VArr = [vArr V];
-    title("Robot Encoder Data");
-    xlabel('X(m)');
-    ylabel('Y(m)');
-%     
     oldenc = newenc;
     t = t + dt;
     pause(0.05);
@@ -124,10 +106,4 @@ function encoderEventListener(handle,event)
     encoderTimeStamp = double(event.Header.Stamp.Sec) + double(event.Header.Stamp.Nsec)/1e9 - tstamp;
     e = [robot.encoders.LatestMessage.Vector.X, robot.encoders.LatestMessage.Vector.Y, encoderTimeStamp];
     newenc = e;
-end
-
-function e = getEncoders()
-    global robot
-%     tstamp = double(event.Header.Stamp.Sec) + double(event.Header.Stamp.Nsec)/1e9;
-%     e = [robot.encoders.LatestMessage.Vector.X, robot.encoders.LatestMessage.Vector.Y, tstamp];
 end
