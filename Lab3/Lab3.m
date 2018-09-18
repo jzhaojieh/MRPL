3e8570274441dc77dd899817a3feabd8be53c4f9
buckenhampalace <42877790+buckenhampalace@users.noreply.github.com>
2018-09-17 22:01:19 -0400

    Add files via upload

--- Lab3/Lab3.m
+++ Lab3/Lab3.m
@@ -1,46 +1,46 @@
 %% Lab 3 Model for Simulation
-% robot = raspbot('sim');
+
 global robot
 global curval
 global tstamp
 global encoderTimeStamp
-robot = raspbot('Raspbot-03');
+robot = raspbot('Raspbot-07');
 pause(2);
 preval = false;
 curval = false;
 tstamp = 0;
-
 global newenc;
+
+
 % initialize constants
 wbase = .088;
 vt = 0.2; sf = 1; tf = sf/vt;
 ktheta = (2*pi)/sf; kk = 15.1084;
 ks = 2; Tf = ks * tf;
 
-% ####### FOR SIM ##############
-% start = tic;
-% t = toc(start);
-% ##############################
 
 % used to store data
-vlArr = []; vrArr = []; dtArr = [];
-xArr = []; yArr = []; VArr = [];
+xArr = []; yArr = [];
 thArr = [];
+
 
 robot.encoders.NewMessageFcn=@encoderEventListener;
 
 prevx = robot.encoders.LatestMessage.Vector.X;
 prevy = robot.encoders.LatestMessage.Vector.Y;
-% ##### Not Sim ##################
+
+
 t = 0;
-% ################################
 oldenc = [0,0,0];
 global myplot;
 x = 0;
 y = 0;
 th = 0;
+newenc = oldenc;
+
 
 while (t < Tf)
+    
     preval = curval;
     st = vt*t/ks;
     angle = ktheta * st;
@@ -51,20 +51,16 @@ while (t < Tf)
     omegat = k * vt;
     vr = vt + (0.044) * omegat;
     vl = vt - (0.044) * omegat;
-    vlArr = [vlArr vl];
-    vrArr = [vrArr vr];
     
     robot.sendVelocity(vl, vr)
-    omega = (vr-vl)/wbase;
-    V = (vr+vl)/2;
-    encoderx = robot.encoders.LatestMessage.Vector.X;
-    encodery = robot.encoders.LatestMessage.Vector.Y;
-    dx = encoderx - prevx;
-    dy = encodery - prevy;
-    prevx = encoderx;
-    prevy = encodery;
-
+    
     if dt ~= 0
+        encoderx = newenc(1);
+        encodery = newenc(2);
+        dx = encoderx - prevx;
+        dy = encodery - prevy;
+        prevx = encoderx;
+        prevy = encodery;
         vlactual = dx/dt;
         vractual = dy/dt;
         Vactual = (vlactual+vractual)/2;
@@ -74,7 +70,6 @@ while (t < Tf)
         y = y + Vactual*sin(th)*dt;
         xArr = [xArr, x];
         yArr = [yArr, y];
-        thArr = [thArr, th];
         plot(yArr, xArr);
         title("Robot Encoder Data");
         xlabel('X(m)');
@@ -84,7 +79,6 @@ while (t < Tf)
     oldenc = newenc;
     t = t + dt;
     pause(0.05);
-    
 end
 
 robot.encoders.NewMessageFcn = [];
