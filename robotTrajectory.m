4ba579e20d8848b1016efc1c2e43d011325237ab
buckenhampalace <42877790+buckenhampalace@users.noreply.github.com>
2018-10-01 20:40:21 -0400

    Add files via upload

--- robotTrajectory.m
+++ robotTrajectory.m
@@ -11,13 +11,14 @@ classdef robotTrajectory < handle
             obj.numsamples = numsamples;
         end
         
-        function res = generateSamples(numsamples)
+        function [tArr, dArr, vArr, pArr] = generateSamples(numsamples)
             V = 0;
             w = 0;
-            si = 0;
-            d = 0;
-            p = [0, 0, 0];
-            res = [];
+            dist = 0;
+            angle = pi/2;
+            x = 0;
+            y = 0;
+            p = [x, y, angle];
             tArr = [];
             dArr = [];
             vArr = [];
@@ -25,10 +26,16 @@ classdef robotTrajectory < handle
             dt = figure8ReferenceControl.refcontrol.totalTime / numsamples;
             for i = 1:(numsamples)
                 ti = (i-1)*dt;
-                [V w] = figure8ReferenceControl.computeControl(obj.refcontrol, ti);
-                vArr = [vArr V];
-                si = si + (v * dt);
-                
+                tArr = [tArr, ti]; %time update
+                [V, w] = figure8ReferenceControl.computeControl(obj.refcontrol, ti);
+                vArr = [vArr V]; %velocity update
+                dist = dist + (V * dt);
+                dArr = [dArr, dist]; %distance update
+                angle = angle + w*dt;
+                x = V*dt*sin(angle);
+                y = V*dt*cos(angle);
+                p = [x, y, angle];
+                pArr = [pArr, p]; %position update
             end
         end
     end
