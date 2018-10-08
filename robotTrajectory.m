4a15b809610a12930294faefaeefa1cd789d7c26
Chris <pseudotechnic@gmail.com>
2018-10-08 17:52:53 -0400

    Add files via upload

--- robotTrajectory.m
+++ robotTrajectory.m
@@ -27,21 +27,24 @@ classdef robotTrajectory < handle
         end
         function generateSamples(obj)
             dt = object.trajTotalTime / object.numSamples;
-            for i = 2:(object.numSamples)
+            for i = 1:(object.numSamples - 1)
                 ti = (i-1)*dt;
                 [V, w] = object.refCon.computeControl(ti);
-                dist = distArr(i-1) + (V*dt);
-                angle = wArr(i-1) + (w*dt);
+                
+                %------THIS PART MIGHT BE WRONG-------
+                angle = wArr(i) + (w*dt);
                 x = V*dt*sin(angle);
                 y = V*dt*cos(angle);
+                %I'M NOT SURE IF THIS IS p(i+1) AS REQUIRED???
                 p = pose(x, y, angle);
+                %--------------------------------------
                 
                 %updates
                 object.timeArr = [object.timeArr, ti];
                 object.velArr = [object.velArr, V];
                 object.wArr = [object.wArr, w];
-                object.distArr = [object.distArr, dist];
-                object.poseArr = [object.poseArr, p];
+                object.distArr(i+1) = object.distArr(i) + (V*dt);
+                object.poseArr(i+1) = p;
             end
         end
         function linVel = getVelocity(obj, t)
