8f642c11a86f7360ac22c2b7edc8a2f56eaa781a
Chris <pseudotechnic@gmail.com>
2018-10-08 17:27:38 -0400

    Add files via upload

--- trajectoryFollower.m
+++ trajectoryFollower.m
@@ -15,23 +15,25 @@ classdef trajectoryFollower < handle
     properties(Access = private)
     end
     methods(Static = true)
-        function obj = trajectoryFollower(robModel, numSamps, referenceControl)
+        function obj = trajectoryFollower(robModel, robotTraject)
             object.rob = robModel;
-            object.W2 = object.rob.W2
-            object.numSamples = numSamps;
-            object.refCon = referenceControl;
-            object.roboTraj = robotTrajectory(object.numSamples, object.refCon);
+            object.W2 = object.rob.W2;
+            object.roboTraj = robotTraject;
         end
         function [vl, vr] = feedForwardVel(obj, t)
             V = object.roboTraj.getVelocity(t)
             w = object.roboTraj.getW(t);
             pose = object.roboTraj.getPoseAtTime(t);
-            vl = V - object.W2*w;
-            vr = V + object.W2*w;
+            
+            %updating arrays
             timeArr = [timeArr, t];
             velArr = [velArr, V];
             wArr = [wArr, w];
             poseArr = [poseArr, pose];
+            
+            %returning velocityLeft and velocityRight
+            vl = V - object.W2*w;
+            vr = V + object.W2*w;
         end
     end
 end
