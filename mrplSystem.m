379bd3096c67eae75cbf9200a6eb7a108bc691e3
Chris <pseudotechnic@gmail.com>
2018-11-01 00:16:43 -0400

    Add files via upload

--- mrplSystem.m
+++ mrplSystem.m
@@ -1,6 +1,8 @@
 classdef mrplSystem < handle
     
     properties
+            vMax = 0.25;
+            aMax = 0.75;
             controllerObj,
             robot,
             trajectoryObj,
@@ -16,6 +18,8 @@ classdef mrplSystem < handle
             tstamp
             newenc
             tprev
+            FaceOffset
+            robFrontOffset
     end
     
     methods
@@ -38,19 +42,110 @@ classdef mrplSystem < handle
             obj.idealPoses = pose(0,0,0);
             
             obj.encoderTimeStamp = 0;
+            obj.robFrontOffset = .08;
+            obj.FaceOffset = .019;
             obj.tstamp = 0;
             obj.newenc = [0,0];
             obj.tprev = 0;
-            obj.robot.encoders.NewMessageFcn=@obj.encoderEventListener;
         end
             
+        function executeTrajectoryToPose(obj, x, y, th, sgn)
+            goal = pose(x, y, th);
+            currentPose = obj.idealPoses(end);
+            convertedGoal = goal.matToPoseVec(currentPose.aToB() * goal.bToA());
+            obj.executeTrajectoryRelativeToPose(convertedGoal(1), convertedGoal(2), convertedGoal(3), sgn);
+        end
+        
         function executeTrajectoryRelativeToPose(obj, x, y, th, sgn)
             obj.trajectoryObj = cubicSpiralTrajectory.planTrajectory(x, y, th, sgn);
             obj.trajectoryObj.planVelocities(0.2);
             obj.executeTrajectory();
         end
         
+%=================From Lab 4=================================        
+        function [uref, tf] = trapezoidalVelocityProfile( t , amax, vmax, dist, sgn)
+        % Returns the velocity command of a trapezoidal profile of maximum
+        % acceleration amax and maximum velocity vmax whose ramps are of
+        % duration tf. Sgn is the sign of the desired velocities.
+        % Returns 0 if t is negative. 
+            tramp = vmax / amax;
+            sf = dist;
+            tf = (sf + vmax^2/amax)/vmax;
 
+            if t < 0 || t >= tf
+                uref = 0;
+            elseif t < tramp
+                uref = amax * t;
+            elseif (tf - t) < tramp
+                uref = amax * (tf - t);
+            elseif tramp < t && t < (tf - tramp)
+                uref = vmax;
+            else
+                uref = 0;
+            end
+            uref = uref * sgn;
+        end
+ 
+        function [rW, lW, tf] = trapezoidalAngVelocityProfile( t , amax, vmax, th)
+        % Returns the velocity command of a trapezoidal profile of maximum
+        % acceleration amax and maximum velocity vmax whose ramps are of
+        % duration tf. Sgn is the sign of the desired velocities.
+        % Returns 0 if t is negative. 
+            tramp = vmax / amax;
+            sf = mod (th, 360);
+            if sf > 180
+                sf = 180-sf;
+            end
+            sgn = 1
+            if sf < 0 
+                sgn = -1
+            end
+            tf = (sf + vmax^2/amax)/vmax;
+
+            if t < 0 || t >= tf
+                uref = 0;
+            elseif t < tramp
+                uref = amax * t;
+            elseif (tf - t) < tramp
+                uref = amax * (tf - t);
+            elseif tramp < t && t < (tf - tramp)
+                uref = vmax;
+            else
+                uref = 0;
+            end
+            rW = uref * sgn;
+            lW = uref;
+        end
+      
+%=====================================================
+               
+        function moveRelDist(obj, dist, doControlPlotting)
+            %move forward or backward a specified distance and stop
+            sgn = 1;
+            if dist < 0
+                sgn = 1;
+            end
+            %make sure the velocity is such that the distance will take at
+            %least a second    
+            [vel, tf] = trapezoidalVelocityProfile(obj.tstamp, obj.aMax, obj.vMax, dist, sgn);
+            if tf >= 1
+            %move distance forward or backward
+                obj.robot.sendVelocity(vel, vel);
+            else
+                disp("moveRelDist had insufficient velocity, takes less than a second");
+            end
+        end
+
+        function turnRelAngle(obj, angle, doControlPlotting)
+            % make sure the velocity is such that the distance will take at
+            % least a second
+            [rw, lw, tf] = trapezoidalAngVelocityProfile(obj.tstamp, obj.aMax, objvMax, angle);
+            if tf >= 1
+                obj.robot.sendVelocity(rw, lw);
+            else
+                disp("turnRelAngle had insufficient velocity, takes less than a second");
+            end
+        
         function executeTrajectory(obj)
             obj.trajFollower = trajectoryFollower(obj.rob, obj.trajectoryObj);
             
@@ -122,13 +217,22 @@ classdef mrplSystem < handle
             obj.robot.stop();
         end
         
-        function encoderEventListener(obj, handle, event)
-            if obj.tstamp == 0
-                obj.tstamp = double(event.Header.Stamp.Sec) + double(event.Header.Stamp.Nsec)/1e9;
-            end
-            obj.encoderTimeStamp = double(event.Header.Stamp.Sec) + double(event.Header.Stamp.Nsec)/1e9;
-            e = [obj.robot.encoders.LatestMessage.Vector.X, obj.robot.encoders.LatestMessage.Vector.Y];
-            obj.newenc = e;
+        %objPoseVec is pose of object relative to sensor
+        function robotGoal = acquisitionPose(obj, objPoseVec, moreOffset)
+             curPose = obj.idealPoses(end);
+             
+             totalDist = obj.robFrontOffset + obj.FaceOffset + moreOffset;
+             xCom = objPoseVec.x;
+             yCom = objPoseVec.y;
+             thCom = objPoseVec.th;
+             xdelt = totalDist * cos(thCom);
+             ydelt = totalDist * sin(thCom);
+             x = xCom + xdelt;
+             y = yCom + ydelt;
+             th = thCom + tan(y/x);
+             goalPose = pose(x,y,th);
+             robotGoal = goalPose.matToPoseVec(curPose.aToB() * goalPose.bToA());
+
         end
     end
 end
